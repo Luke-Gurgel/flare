315e5e03354352769ec6fb9548c0430ae1224086
Lucas Gurgel <luke.gur110@gmail.com>
2019-04-28 17:55:48 -0400

    Setup redux #6

---
+++ src/redux/theme/index.ts
@@ -0,0 +1,25 @@
+export enum ThemeActionTypes {
+  switchTheme = "switchTheme",
+}
+
+export interface SwitchThemeAction {
+  type: ThemeActionTypes.switchTheme
+}
+
+export const themeActions = {
+  switchTheme: (): SwitchThemeAction => ({
+    type: ThemeActionTypes.switchTheme,
+  }),
+}
+
+export interface ThemeState {
+  readonly isDark: boolean
+}
+
+export const themeState: ThemeState = {
+  isDark: false,
+}
+
+export const themeReducer = (state = themeState, action: SwitchThemeAction): ThemeState => {
+  return action.type === ThemeActionTypes.switchTheme ? { isDark: !state.isDark } : state
+}
