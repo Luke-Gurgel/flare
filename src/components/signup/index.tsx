3252ff689d942e0cf862805c3d17adc9e1e8ef96
Luke Gurgel <luke.gurgel@track.com>
2019-05-10 21:39:31 -0400

    Add Signup page

---
+++ src/components/signup/index.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { StatusBar } from "react-native"
+import { Page, PageHeader } from "src/components/common/index"
+import { PageTitle, Message, Description } from "./styled"
+import SignupForm from "./SignupForm"
+import { NavigationProps } from "src/types/index"
+
+const Signup = ({ navigation }: NavigationProps) => {
+  const goBack = () => {
+    navigation.goBack && navigation.goBack()
+  }
+
+  return (
+    <Page>
+      <StatusBar barStyle="default" />
+      <PageHeader
+        goBack={goBack}
+        centerComponent={<PageTitle>Sign up</PageTitle>}
+      />
+      <Message>Create an account</Message>
+      <Description>In order to post tasks or apply to them</Description>
+      <SignupForm />
+    </Page>
+  )
+}
+
+export default Signup
