a3b46932cd78b84de2a18de828e352dfe62f2d66
Luke Gurgel <luke.gurgel@track.com>
2019-05-18 20:36:53 -0400

    Add redux dev tools

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import React from "react"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "styled-components/native"
-import instantiateStore from "./src/redux/store"
+import createStore from "./src/redux/store"
 import themes from "src/styles/themes/index"
 import OnboardingNavigator from "src/components/navigators/OnboardingNavigation"
 
-const store = instantiateStore()
+const store = createStore()
 
 const App: React.FC = () => (
   <Provider store={store}>
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,14 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware } from "redux"
+import { composeWithDevTools } from "remote-redux-devtools"
+import thunk from "redux-thunk"
 import rootReducer from "./rootReducer"
 import initialState from "./initialState"
 
-export default () => createStore(rootReducer, initialState)
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 8081 })
+
+export default () =>
+  createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk)),
+  )
