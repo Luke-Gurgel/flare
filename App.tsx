7864da52f64ee780b2cca2d43e6a0f65e4f64392
Luke Gurgel <luke.gurgel@track.com>
2019-05-25 20:29:47 -0400

    Add ThemeProvider to Bootstrap component

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 import React from "react"
 import { Provider } from "react-redux"
-import { ThemeProvider } from "styled-components/native"
 import createStore from "./src/redux/store"
-import themes from "src/styles/themes/index"
 import Bootstrap from "src/components/navigators/Bootstrap"
 
 const store = createStore()
 
 const App: React.FC = () => (
   <Provider store={store}>
-    <ThemeProvider theme={themes.light}>
-      <Bootstrap />
-    </ThemeProvider>
+    <Bootstrap />
   </Provider>
 )
 
--- src/components/navigators/Bootstrap.tsx
+++ src/components/navigators/Bootstrap.tsx
@@ -1,19 +1,27 @@
 import React from "react"
 import { connect } from "react-redux"
 import { InitialState } from "src/redux/initialState"
+import { ThemeProvider } from "styled-components/native"
+import themes from "src/styles/themes"
 import RootNavigator from "./RootNavigator"
 import HomeNavigator from "./HomeNavigator"
 
 interface MapStateProps {
   loggedIn: boolean
+  isDark: boolean
 }
 
-const Bootstrap = ({ loggedIn }: MapStateProps) => {
-  return loggedIn ? <HomeNavigator /> : <RootNavigator />
+const Bootstrap = ({ loggedIn, isDark }: MapStateProps) => {
+  return (
+    <ThemeProvider theme={isDark ? themes.dark : themes.light}>
+      {loggedIn ? <HomeNavigator /> : <RootNavigator />}
+    </ThemeProvider>
+  )
 }
 
 const mapState = (state: InitialState): MapStateProps => ({
   loggedIn: state.userAuth.loggedIn,
+  isDark: state.theme.isDark,
 })
 
 export default connect(mapState)(Bootstrap)
