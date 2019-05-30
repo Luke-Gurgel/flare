2b087600726c626271c3504713fe6688bc6f0a12
Luke Gurgel <luke.gurgel@track.com>
2019-05-29 21:20:09 -0400

    Add common RequestStatus interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,5 @@
 import ThemeProps from "./ThemeProps"
 import ButtonProps from "./ButtonProps"
+import RequestState from "./RequestState"
 
-export { ThemeProps, ButtonProps }
+export { ThemeProps, ButtonProps, RequestState }
